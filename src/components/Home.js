64ca8aa686446576830f4020d68072569f74831d
thehasanovv <hasanov.e95@gmail.com>
2022-03-07 01:04:26 +0400

    add slick-carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,33 @@
-import React from "react";
+import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <ImgSlider></ImgSlider>
+    </Container>
+  );
 };
 
 export default Home;
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+  &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
